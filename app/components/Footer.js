61166f070b41e6af504b2366b53938ba0486e8ee
chirag bhandar <chirag0space@gmail.com>
2025-12-06 12:40:35 +0530

    doneeee

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full bg-black text-gray-400 py-8 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm">
-          © 2025 Chirag Bhandar.{' '}
+          © 2025 Chirag Bhandar . {' '}
           <span className="underline">Crafted by yours truly</span>
         </p>
       </div>
